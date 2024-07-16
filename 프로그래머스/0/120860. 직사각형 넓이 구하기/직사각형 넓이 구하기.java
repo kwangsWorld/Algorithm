ab26d0c6f903e18c8d8c7601656044d12890a895
kwangsWorld <43844533+kwangsWorld@users.noreply.github.com>
2024-07-16 17:47:20 +0900

    [level 0] Title: 직사각형 넓이 구하기, Time: 25.79 ms, Memory: 89.1 MB -BaekjoonHub

--- 프로그래머스/0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.java
+++ 프로그래머스/0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.java
@@ -1,29 +1,38 @@
 class Solution {
     public int solution(int[][] dots) {
-        // 초기값을 첫 번째 점의 좌표로 설정합니다.
-        int xMin = dots[0][0], xMax = dots[0][0];
-        int yMin = dots[0][1], yMax = dots[0][1];
-        
-        // 모든 점을 순회하면서 xMin, xMax, yMin, yMax 값을 갱신합니다.
+        int answer = 0;
+        int xMax = 0, xMin = 0, yMax = 0, yMin = 0;
         for(int i = 0; i < dots.length; i++){
-            int x = dots[i][0];
-            int y = dots[i][1];
-            
-            // x 좌표의 최소, 최대값 갱신
-            if(x < xMin) xMin = x;
-            if(x > xMax) xMax = x;
-            
-            // y 좌표의 최소, 최대값 갱신
-            if(y < yMin) yMin = y;
-            if(y > yMax) yMax = y;
-            
-            // 디버깅 출력
-            System.out.println("점 (" + x + ", " + y + ")");
-            System.out.println("xMin: " + xMin + ", xMax: " + xMax + ", yMin: " + yMin + ", yMax: " + yMax);
-        }
-        
-        // 넓이를 계산합니다.
-        int answer = (xMax - xMin) * (yMax - yMin);
+            for(int j = 0; j < dots[i].length; j++){
+                System.out.println("i값 : " + i + " / j값 : " + j +
+                                     " / dot[i][j]의 값: " + dots[i][j]);
+                if(i == 0 && j == 0){
+                    xMin = dots[i][j]; 
+                    xMax = dots[i][j];
+                }
+                if(i == 0 && j == 1){
+                    yMin = dots[i][j];
+                    yMax = dots[i][j];
+                }
+                if(j == 0){
+                    if(xMin > dots[i][j]){
+                        xMin = dots[i][j];
+                    }
+                    if(xMax < dots[i][j]){
+                        xMax = dots[i][j];
+                    }
+                }
+                if(j == 1){
+                    if(yMin > dots[i][j]){
+                        yMin = dots[i][j];
+                    }
+                    if(yMax < dots[i][j]){
+                        yMax = dots[i][j];
+                    }
+                }
+            }
+        }// x, y값 끝
+        answer = (xMax - xMin) * (yMax - yMin);
         
         return answer;
     }
