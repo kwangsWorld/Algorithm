c456053fec7e5a97feb679fb1ad2bcb94552d4b1
kwangsWorld <43844533+kwangsWorld@users.noreply.github.com>
2025-05-10 23:23:50 +0900

    [level 0] Title: 대소문자 바꿔서 출력하기, Time: 189.86 ms, Memory: 68.1 MB -BaekjoonHub

--- 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.java
+++ 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.java
@@ -3,20 +3,17 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String a = sc.next();
-		int cnt = a.length();
-		char[] arr = new char[cnt];
-		for(int i = 0; i < cnt; i++) {
-			arr[i] = a.charAt(i);
-			int num = arr[i];
-			if(num >= 65 && num <= 90) {
-				num += 32;
-			}
-			else {
-				num -= 32;
-			}
-			arr[i] = (char)num;
-		}
-		System.out.println(arr);
-	}
+        String str = sc.next();
+        for(int i = 0; i < str.length(); i++){
+            int number = (int)str.charAt(i);
+            // System.out.print("형변환 전 number = " + number);
+            if(number >= 65 && number <= 90){
+                number += 32;
+            }else{
+                number -= 32;
+            }
+            // System.out.println("/ 형변환 후 number = " + number);
+            System.out.print((char)number);
+        }
+    }
 }
