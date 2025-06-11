d1f4936952496310f9bdf8a9be2bd13aa314afef
kwangsWorld <43844533+kwangsWorld@users.noreply.github.com>
2025-06-11 18:47:04 +0900

    [level 0] Title: 배열 만들기 4, Time: 23.23 ms, Memory: 79.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.java
@@ -0,0 +1,27 @@
+import java.util.List;
+import java.util.ArrayList;
+
+class Solution {
+    public int[] solution(int[] arr) {
+        
+        List<Integer> list = new ArrayList<>(); 
+        
+        for(int i = 0; i < arr.length; i++){
+            if(list.size() == 0){
+                list.add(arr[i]);
+            }else if(list.get(list.size()-1) < arr[i]){
+                list.add(arr[i]);
+            }else if(list.get(list.size()-1) >= arr[i]){
+                list.remove(list.size()-1);
+                i--;
+            }
+        }
+        
+        int[] stk = new int[list.size()];
+        for(int i = 0; i < list.size(); i++){
+            stk[i] = list.get(i);
+        }
+        
+        return stk;
+    }
+}
