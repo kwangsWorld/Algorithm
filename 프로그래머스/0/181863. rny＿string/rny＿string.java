a61c1613a1dc7116436128d07577615eabbfca7b
kwangsWorld <43844533+kwangsWorld@users.noreply.github.com>
2025-07-27 00:10:13 +0900

    [level 0] Title: rny_string, Time: 0.04 ms, Memory: 83.4 MB -BaekjoonHub

--- 프로그래머스/0/181863. rny＿string/rny＿string.java
+++ 프로그래머스/0/181863. rny＿string/rny＿string.java
@@ -1,10 +1,8 @@
 class Solution {
     public String solution(String rny_string) {
         String answer = "";
-        String m = "m";
-        String rn = "rn";
         
-        answer = rny_string.replace(m,rn);
+        answer = rny_string.replace("m","rn");
         return answer;
     }
 }
