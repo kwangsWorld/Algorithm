6b15aeaa47616e08d9cf560b13614fea36cc3278
kwangsWorld <43844533+kwangsWorld@users.noreply.github.com>
2025-08-09 00:44:22 +0900

    [level 0] Title: 배열 만들기 6, Time: 39.47 ms, Memory: 147 MB -BaekjoonHub

---
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+
+class Solution {
+    public int[] solution(int[] arr) {
+        int[] answer = {};
+        
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        for(int i = 0; i < arr.length;){
+            if(list.size() == 0){
+                list.add(arr[i]);
+                i++;
+            }else if(list.size() > 0 && list.get(list.size() - 1) == arr[i]){
+                list.remove(list.size() - 1);
+                i++;
+            }else{
+                list.add(arr[i]);
+                i++;
+            }
+        }
+        
+        answer = new int[list.size()];
+        for(int i = 0; i < list.size(); i++){
+            answer[i] = list.get(i);
+        }
+        
+        if(answer.length == 0){
+            return new int[]{-1};
+        }
+        return answer;
+    }
+}
