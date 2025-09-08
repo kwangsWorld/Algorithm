8556907468a1ead129fb9da5e0928d11eefee126
kwangsWorld <43844533+kwangsWorld@users.noreply.github.com>
2025-09-09 00:32:38 +0900

    [level 0] Title: 정사각형으로 만들기, Time: 0.37 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/0/181830. 정사각형으로 만들기/정사각형으로 만들기.java
+++ 프로그래머스/0/181830. 정사각형으로 만들기/정사각형으로 만들기.java
@@ -4,30 +4,16 @@ class Solution {
         int row = arr.length;
         int column = arr[0].length;     
         
-        if(row == column){
-            int[][] answer = new int[row][column];
-            for(int i = 0; i < row; i++){
-                for(int j = 0; j < column; j++){
-                    answer[i][j] = arr[i][j];
-                }
+        int size = Math.max(row,column);
+        
+        int[][] answer = new int[size][size];
+        
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < column; j++){
+                answer[i][j] = arr[i][j];
             }
-            return answer;
-        }else if(row > column){
-            int[][] answer = new int[row][row];
-            for(int i = 0; i < row; i++){
-                for(int j = 0; j < column; j++){
-                    answer[i][j] = arr[i][j];
-                }
-            }
-            return answer;
-        }else{
-             int[][] answer = new int[column][column];
-             for(int i = 0; i < row; i++){
-                for(int j = 0; j < column; j++){
-                    answer[i][j] = arr[i][j];
-                }
-            }
-            return answer;
         }
+        
+        return answer;
     }
 }
