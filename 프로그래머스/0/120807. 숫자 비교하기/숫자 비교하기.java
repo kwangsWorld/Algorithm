6c32715b8ed148ac699d4f6eded18f195b6b9f0d
kwangsWorld <43844533+kwangsWorld@users.noreply.github.com>
2025-09-15 00:00:18 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.02 ms, Memory: 103 MB -BaekjoonHub

--- 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.java
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.java
@@ -1,11 +1,8 @@
 class Solution {
     public int solution(int num1, int num2) {
-        int answer = 0;
-        if(num1 == num2){
-            answer = 1;
-        }else{
-            answer = -1;
-        }
+        int answer = -1;
+        
+        if(num1 == num2){answer = 1;}
         return answer;
     }
 }
