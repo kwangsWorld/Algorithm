635498f7b7162630e59a59d0c0f70e0f44a2bf32
kwangsWorld <43844533+kwangsWorld@users.noreply.github.com>
2025-09-25 13:54:40 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.02 ms, Memory: 87.2 MB -BaekjoonHub

--- 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
+++ 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
@@ -1,9 +1,13 @@
 class Solution {
     public int solution(int n) {
-        if( n % 7 == 0){
-            return n / 7;
+        int answer = 0;
+        if(n <= 7){
+            answer = 1;
+        }else if(n % 7 == 0){
+            answer = n / 7;
         }else{
-            return n / 7 + 1;
+            answer = (n / 7) + 1;
         }
+        return answer;
     }
 }
