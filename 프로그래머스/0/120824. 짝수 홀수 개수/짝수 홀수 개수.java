12b8dc98b2e8e722479fc8384f528e54c93d4c4b
kwangsWorld <43844533+kwangsWorld@users.noreply.github.com>
2025-10-14 09:06:45 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.01 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[2];
+        
+        int oddCount = 0;
+        int evenCount = 0;
+        
+        for(int i : num_list){
+            if(i % 2 == 0){
+                evenCount++;
+            }else{
+                oddCount++;
+            }
+        }
+        
+        answer[0] = evenCount;
+        answer[1] = oddCount;
+        
+        return answer;
+    }
+}
