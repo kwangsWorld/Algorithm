b607c7d2994739d8704025dbe931c9a3881ab4dd
kwangsWorld <43844533+kwangsWorld@users.noreply.github.com>
2025-10-24 23:59:45 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.02 ms, Memory: 84.5 MB -BaekjoonHub

--- 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.java
+++ 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.java
@@ -1,22 +1,18 @@
-import java.util.Arrays;
-
 class Solution {
     public int[] solution(int[] emergency) {
         int[] answer = new int[emergency.length];
-        int[] temp = new int[emergency.length];
-        
-        for(int i = 0; i < emergency.length; i++){
-            temp[i] = emergency[i];
-        }
-        
-        Arrays.sort(temp);
-        
-        for(int i = 0; i < temp.length; i++){
-            for(int j = 0; j < temp.length; j++){
-                if(temp[i] == emergency[j]){
-                    answer[j] += temp.length - i;
+
+        for(int i = 0; i < answer.length; i++){
+            if(answer[i] != 0){
+                continue;
+            }
+            int idx = 1;
+            for(int j = 0; j < answer.length; j++){
+                if(emergency[i] < emergency[j]){
+                    idx++;
                 }
             }
+            answer[i] = idx;
         }
         return answer;
     }
