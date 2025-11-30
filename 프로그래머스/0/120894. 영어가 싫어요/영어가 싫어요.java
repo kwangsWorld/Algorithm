c329e79fbdd721bfacdb452a0170ad6b57f90be4
kwangsWorld <43844533+kwangsWorld@users.noreply.github.com>
2025-12-01 00:56:08 +0900

    [level 0] Title: 영어가 싫어요, Time: 2.23 ms, Memory: 85.2 MB -BaekjoonHub

--- 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.java
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.java
@@ -1,45 +1,52 @@
 class Solution {
     public long solution(String numbers) {
         String answer = "";
-        for(int i = 0; i < numbers.length();){
-            if(numbers.charAt(i) == 'o'){
-                answer += '1';
-                i += 3;
-            }else if(numbers.charAt(i) == 't'){
-                if(numbers.charAt(i+1) == 'w'){
-                    answer += '2';
-                    i += 3;
+        
+            while(true){
+                if(numbers.charAt(0) == 'o'){
+                    answer += "1";
+                    numbers = numbers.substring(3);
+                }else if(numbers.charAt(0) == 't'){
+                    if((numbers.substring(0,5)).equals("three")){
+                        answer += "3";
+                        numbers = numbers.substring(5);
+                    }else{
+                        answer += "2";
+                        numbers = numbers.substring(3);
+                    }
+                }else if(numbers.charAt(0) == 'f'){
+                    if(numbers.substring(0,4).equals("four")){
+                        answer += "4";
+                        numbers = numbers.substring(4);
+                    }else{
+                        answer += "5";
+                        numbers = numbers.substring(4);
+                    }
+                }else if(numbers.charAt(0) == 's'){
+                    if(numbers.substring(0,3).equals("six")){
+                        answer += "6";
+                        numbers = numbers.substring(3);
+                    }else{
+                        answer += "7";
+                        numbers = numbers.substring(5);
+                    }
+                }else if(numbers.charAt(0) == 'e'){
+                    answer += "8";
+                    numbers = numbers.substring(5);
+                }else if(numbers.charAt(0) == 'n'){
+                    answer += "9";
+                    numbers = numbers.substring(4);
                 }else{
-                    answer += '3';
-                    i += 5;
+                    answer+= "0";
+                    numbers = numbers.substring(4);
                 }
-            }else if(numbers.charAt(i) == 'f'){
-                if(numbers.charAt(i+1) == 'o'){
-                    answer += '4';
-                    i += 4;
-                }else{
-                    answer += '5';
-                    i += 4;
+                
+                if(numbers.equals("")){
+                    break;
                 }
-            }else if(numbers.charAt(i)== 's'){
-                if(numbers.charAt(i+1) == 'i'){
-                    answer += '6';
-                    i += 3;
-                }else{
-                    answer += '7';
-                    i += 5;
-                }
-            }else if(numbers.charAt(i) == 'e'){
-                answer += '8';
-                i += 5;
-            }else if(numbers.charAt(i) == 'n'){
-                answer += '9';
-                i += 4;
-            }else{
-                answer += '0';
-                i += 4;
+
             }
-        }
+        
         return Long.parseLong(answer);
     }
 }
